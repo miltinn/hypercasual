d0e5adf9e1cb62074660b44f7c17ca2e91ad5c7e
Milton <augusto.milton99@gmail.com>
2024-08-01 18:06:41 -0300

    added level piece randomization and prefabs

---
+++ Assets/Scripts/ArtPiece/ArtManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ebac.Core.Singleton;
+
+public class ArtManager : Singleton<ArtManager>
+{
+    public enum ArtType
+    {
+        TYPE_01,
+        TYPE_02,
+        TYPE_03,
+        BEACH,
+        SNOW
+    }
+
+    public List<ArtSetup> artSetups;
+
+    public ArtSetup GetSetupByType(ArtType artType)
+    {
+        return artSetups.Find(i => i.artType == artType);
+    }
+}
+
+[System.Serializable]
+public class ArtSetup
+{
+    public ArtManager.ArtType artType;
+    public GameObject gameObject;
+}
---
+++ Assets/Scripts/LevelManager/LevelManager.cs
@@ -0,0 +1,127 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public Transform container;
+
+    public List<GameObject> levels;
+
+    public List<LevelPieceBasedSetup> levelPieceBasedSetups;
+    public float timeBetweenPieces = .3f;
+
+    [SerializeField] private int _index;
+    private GameObject _currentLevel;
+
+    private List<LevelPieceBase> _spawnedPieces = new List<LevelPieceBase>();
+    private LevelPieceBasedSetup _currSetup;
+
+
+    private void Awake()
+    {
+        //SpawnNextLevel();
+        CreateLevelPieces();
+    }
+    private void SpawnNextLevel()
+    {
+        if (_currentLevel != null)
+        {
+            Destroy(_currentLevel);
+            _index++;
+            if (_index >= levels.Count)
+            {
+                ResetLevelIndex();
+            }
+        }
+
+        _currentLevel = Instantiate(levels[_index], container);
+        _currentLevel.transform.localPosition = Vector3.zero;
+    }
+
+    private void ResetLevelIndex()
+    {
+        _index = 0;
+    }
+
+    #region
+    private void CreateLevelPieces()
+    {
+        StartCoroutine(CreateLevelPiecesCoroutine());
+    }
+
+    private void CreateLevelPiece(List<LevelPieceBase> list)
+    {
+        var piece = list[Random.Range(0, list.Count)];
+        var spawnedPiece = Instantiate(piece, container);
+
+        if(_spawnedPieces.Count > 0) //not the first piece
+        {
+            var lastPiece = _spawnedPieces[_spawnedPieces.Count - 1];
+            spawnedPiece.transform.position = lastPiece.endPiece.position;
+        }
+
+        foreach(var p in spawnedPiece.GetComponentsInChildren<ArtPiece>())
+        {
+            p.ChangePiece(ArtManager.Instance.GetSetupByType(_currSetup.artType).gameObject);
+        }
+
+        _spawnedPieces.Add(spawnedPiece);
+    }
+
+    private void CleanSpawnedPieces()
+    {
+        for (int i = _spawnedPieces.Count - 1; i >= 0; i--)
+        {
+            Destroy(_spawnedPieces[i].gameObject);
+        }
+
+        _spawnedPieces.Clear();
+    }
+
+    IEnumerator CreateLevelPiecesCoroutine()
+    {
+        CleanSpawnedPieces();
+
+        if (_currSetup != null)
+        {
+            Destroy(_currentLevel);
+            _index++;
+
+            if (_index >= levelPieceBasedSetups.Count)
+            {
+                ResetLevelIndex();
+            }
+        }
+
+        _currSetup = levelPieceBasedSetups[_index];
+
+        for (int i = 0; i < _currSetup.piecesStartNumber; i++)
+        {
+            CreateLevelPiece(_currSetup.levelPiecesStart);
+            yield return new WaitForSeconds(timeBetweenPieces);
+        }
+
+        for (int i = 0; i < _currSetup.piecesNumber; i++)
+        {
+            CreateLevelPiece(_currSetup.levelPieces);
+            yield return new WaitForSeconds(timeBetweenPieces);
+        }
+
+        for (int i = 0; i < _currSetup.piecesEndNumber; i++)
+        {
+            CreateLevelPiece(_currSetup.levelPiecesEnd);
+            yield return new WaitForSeconds(timeBetweenPieces);
+        }
+    }
+    #endregion
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            CreateLevelPieces();
+        }
+    }
+}
+
